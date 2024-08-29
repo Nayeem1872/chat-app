316bb8f3bb9d414c452da33d52010d8424697791
“Nayeem1872” <nayeem.islam.1872@gmail.com>
2024-08-29 20:18:07 +0600

    first commit

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
+require("dotenv").config(); // Import and configure dotenv
 
 const app = express();
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 4000; // Use PORT from .env or fallback to 4000
 
-const server = app.listen(PORT, () => console.log(`server is running on port ${PORT}`));
+const server = app.listen(PORT, () =>
+  console.log(`Server is running on port ${PORT}`)
+);
 
-const io = require('socket.io')(server);
-let socketsConnected = new Set()
+const io = require("socket.io")(server);
+let socketsConnected = new Set();
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-io.on("connection",(onConnected));
+io.on("connection", onConnected);
 
-function onConnected(socket){
-    console.log(socket.id);
-    socketsConnected.add(socket.id)
+function onConnected(socket) {
+  console.log(socket.id);
+  socketsConnected.add(socket.id);
 
-    io.emit('clients-total', socketsConnected.size)
+  io.emit("clients-total", socketsConnected.size);
 
-    socket.on("disconnect",()=>{
-        console.log("socket disconnected", socket.id);
-        socketsConnected.delete(socket.id)
-        io.emit('clients-total', socketsConnected.size)
-        
-    })
+  socket.on("disconnect", () => {
+    console.log("socket disconnected", socket.id);
+    socketsConnected.delete(socket.id);
+    io.emit("clients-total", socketsConnected.size);
+  });
 
-    socket.on('message',(data)=>{
-        console.log(data);
-        socket.broadcast.emit("chat-message",data)
-        
-    })
+  socket.on("message", (data) => {
+    console.log(data);
+    socket.broadcast.emit("chat-message", data);
+  });
 
-
-    socket.on("feedback",(data)=>{
-        socket.broadcast.emit("feedback",data)
-    })
+  socket.on("feedback", (data) => {
+    socket.broadcast.emit("feedback", data);
+  });
 }
